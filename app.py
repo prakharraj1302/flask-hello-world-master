b5f98729e48b27ec3a8aa8f4f01c5195437a1e0b
prakharraj1302 <63489114+prakharraj1302@users.noreply.github.com>
2023-10-29 15:34:53 +0530

    add post

--- app.py
+++ app.py
@@ -1,6 +1,58 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+
+
+from flask import Flask, request, jsonify
+import requests
+
 app = Flask(__name__)
 
+#1
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+#2
+
+@app.route('/get_leetcode_profile', methods=['POST'])
+def get_leetcode_profile():
+    # Extract username from the request's JSON body
+    data = request.json
+    username = data.get("username")
+
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    # GraphQL query and variables
+    graphql_query = {
+        "query": """
+        query getUserProfile($username: String!) {
+            allQuestionsCount { difficulty count }
+            matchedUser(username: $username) {
+                contributions { points }
+                profile { reputation ranking }
+                submissionCalendar
+                submitStats {
+                    acSubmissionNum { difficulty count submissions }
+                    totalSubmissionNum { difficulty count submissions }
+                }
+            }
+        }
+        """,
+        "variables": {"username": username}
+    }
+
+    # Headers for the POST request to LeetCode
+    headers = {
+        "referer": f"https://leetcode.com/{username}/",
+        "Content-Type": "application/json"
+    }
+
+    # URL for LeetCode GraphQL API
+    url = "https://leetcode.com/graphql/"
+
+    # Making the POST request to LeetCode
+    response = requests.post(url, json=graphql_query, headers=headers)
+
+    # Return the response from LeetCode
+    return jsonify(response.json())
